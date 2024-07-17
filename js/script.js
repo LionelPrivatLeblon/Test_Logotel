fce0a638482faf340b2ae1686716e769a430ef81
Lionel <l.privatleblon@gmail.com>
2024-07-17 22:40:32 +0200

    Navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function test() {
-  console.log("test");
-}
-test();
+window.addEventListener('scroll', function () {
+  var navbar = document.getElementById('navbar');
+  var sticky = window.innerHeight;
+
+  if (window.scrollY > sticky) {
+    navbar.classList.add('fixed');
+  } else {
+    navbar.classList.remove('fixed');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var navLinks = document.querySelectorAll('.navRight a');
+
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      navLinks.forEach(function (link) {
+        link.classList.remove('active');
+      });
+      this.classList.add('active');
+    });
+  });
+});
